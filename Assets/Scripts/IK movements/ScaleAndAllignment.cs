69cae02973095b17b35c8d732ad7c5b44534c4b6
timofeir1000 <timofei.r1000@gmail.com>
2026-06-14 15:52:18 +0200

    changed vr/pc setup to a more reliable. Added Idle psing (WIP). tarted working on ragdolls (WIP)

--- Assets/Scripts/IK movements/ScaleAndAllignment.cs
+++ Assets/Scripts/IK movements/ScaleAndAllignment.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using FishNet.Object;
+using UnityEngine.XR;
 
 public class ScaleAndAllignment : NetworkBehaviour
 {
     [SerializeField] private Transform rightHand;
     [SerializeField] private Transform leftHand;
+    [SerializeField] private Transform rightHandIdlePos;
+    [SerializeField] private Transform leftHandIdlePos;
     [SerializeField] private Transform body;
     [SerializeField] private Transform EmptyCamera;
     [SerializeField] private Transform avatar;
@@ -14,8 +17,11 @@ public class ScaleAndAllignment : NetworkBehaviour
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float CameraToHeadOffset;
 
-    private bool calibrationDone = true;
+    private bool calibrationDone;
     private float playerHeight;
+    public bool playerIsThere;
+    public bool calibratedToVr;
+    private InputDevice headset;
 
     // in unity units (meters)
     [SerializeField] private float AvatarHeight;
@@ -40,31 +46,59 @@ public class ScaleAndAllignment : NetworkBehaviour
         }
     }
 
+    
     IEnumerator Start()
     {
         yield return new WaitUntil(() => EmptyCamera.localPosition.y > 0.1f);
-        CharachterCalibration();
+        //CharachterCalibration();
+        headset = InputDevices.GetDeviceAtXRNode(XRNode.Head);
+        if (headset.isValid)
+        {
+            if (calibrationDone == false)
+            {
+                CharachterCalibration();
+                print("calibrated to VR");
+            }
+        }
+        else
+        {
+            if (calibrationDone == false)
+            {
+                PcCharachterCalibration();
+                print("calibrated to PC");
+            }
+        }
     }
 
     void Update()
     {
-        RotateTheBody();
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+        if (headset.TryGetFeatureValue(CommonUsages.userPresence, out playerIsThere) && headset.isValid)
         {
-            PcCharachterCalibration();
+            if (!playerIsThere)
+            {
+                idlePose();
+            }
         }
 
         if (calibrationDone == true)
         {
             AlignAvatarToCamera();
+
+
+            /*
+            if (calibratedToVr == true && playerIsThere == false)
+            {
+                idlePose();
+            }
+            */
         }
+        RotateTheBody();
     }
 
     void RotateTheBody()
     {
         Vector3 _headForward = EmptyCamera.forward;
 
-        print("_headForward: " +  _headForward);
         _headForward.y = 0;
         _headForward.Normalize();
 
@@ -79,10 +113,13 @@ public class ScaleAndAllignment : NetworkBehaviour
     void CharachterCalibration()
     {
         playerHeight = EmptyCamera.transform.localPosition.y;
-        float _scale = playerHeight / AvatarHeight * 1f; 
+        print("player height: " + playerHeight);
+        print("EmptyCamera.transform.localPosition.y " + EmptyCamera.transform.localPosition.y);
+        float _scale = playerHeight / AvatarHeight * 1f;
         avatar.localScale = Vector3.one * _scale;
         PCmovementScript.enabled = false;
         calibrationDone = true;
+        calibratedToVr = true;
     }
 
     void PcCharachterCalibration()
@@ -101,5 +138,12 @@ public class ScaleAndAllignment : NetworkBehaviour
         _newAvatarPos.z = _EmptyCamera.z;
         _newAvatarPos.y = _EmptyCamera.y - CameraToHeadOffset;
         avatar.position = Vector3.Lerp(avatar.position, _newAvatarPos, Time.deltaTime * 100f);
+    }
+
+    void idlePose()
+    {
+        //rightHand = rightHandIdlePos;
+        //leftHand = leftHandIdlePos;
+        EmptyCamera.localPosition = new Vector3(0f, playerHeight, 0f);
     }
 }
